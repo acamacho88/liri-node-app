5cbb17fd4a052723a77380d7d970637f4e2b3918
Alex Camacho <alex.c@WalkMe.local>
2018-07-28 09:29:58 -0700

    Got spotify working

--- liri.js
+++ liri.js
@@ -30,6 +30,30 @@ if (process.argv.length > 2) {
         case 'my-tweets':
             break;
         case 'spotify-this-song':
+            var song = '';
+            if (process.argv.length > 3) {
+                song = process.argv[3];
+            } else {
+                song = 'The Sign';
+            }
+            if (process.argv.length > 4) {
+                for (var i = 4; i < process.argv.length; i++) {
+                    song += ' ' + process.argv[i];
+                }
+            }
+            console.log(song);
+            spotify.search({
+                type: 'track',
+                query: song
+            }, function (err, data) {
+                if (err) console.log(err);
+                var songData = data.tracks.items[0];
+                if (song == 'The Sign') songData = data.tracks.items[5];
+                console.log('Artist: ' + songData.artists[0].name + '\n' +
+                    'Song name: ' + songData.name + '\n' +
+                    'Preview url: ' + (songData.preview_url !== null ? songData.preview_url : 'none available') + '\n' +
+                    'Album: ' + songData.album.name);
+            })
             break;
         case 'movie-this':
             var movieName = '';
